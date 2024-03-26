06f126e5d39b1b8b33bb8b676a88d09e1e2c56bd
r.b4rr10s <rodrigoedu11@gmail.com>
2024-03-25 20:58:36 -0500

    Primer ejercicio lab

---
+++ Lab1/actividad1.py
@@ -0,0 +1,36 @@
+import paramiko
+
+
+def connection(host, username, password):
+    byte_dict = dict()
+    cmd1 = "echo -n $(hostname)"
+    cmd2 = "ip -br addr | grep -v lo | cut -d \" \" -f 1 | awk '{print $0}'"
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password)
+    _stdin, _stdout, _stderr = client.exec_command(cmd1)
+    hostname = _stdout.read().decode()
+    _stdin, _stdout, _stderr = client.exec_command(cmd2)
+    ifaces = _stdout.read().decode()
+    for iface in ifaces.splitlines():
+        cmd3 = "ip -s link show " + iface + " | tail -n 1 | awk '{print $1}'"
+        _stdin, _stdout, _stderr = client.exec_command(cmd3)
+        bytes_tx = _stdout.read().decode()
+        byte_dict[iface] = bytes_tx
+    return hostname, byte_dict
+
+    client.close()
+
+
+if __name__ == "__main__":
+
+    username = "ubuntu"
+    password = "ubuntu"
+    ips = ["30", "40", "50"]
+
+    # Workers info
+    for i in ips:
+        host = f"10.0.0.{i}"
+        hostname, byte_dict = connection(host, username, password)
+        print(hostname)
+        print(byte_dict)
