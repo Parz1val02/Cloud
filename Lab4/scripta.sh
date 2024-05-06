c6abb85b10b64ffdb276d7bb4340283fdbf36f03
r.b4rr10s <rodrigoedu11@gmail.com>
2024-05-05 21:53:44 -0500

    Ya esta el script 1 xd

--- Lab4/scripta.sh
+++ Lab4/scripta.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-existe_OVS() {
+existe_ovs() {
 	ovs-vsctl br-exists "$1"
 	return $?
 }
-if ! existe_OVS "$?"; then
-    echo "crearlo"
+if ! existe_ovs "$?"; then
+	ovs-vsctl add-br "$1"
+	echo "OVS $1 creado"
 else
-    echo "creado"
+	echo "OVS $1 ya existe"
 fi
+for iface in "$@"; do
+	if [ "$iface" == "$1" ]; then
+		continue
+	fi
+	ovs-vsctl add-port "$1" "$iface"
+done
+echo 1 >/proc/sys/net/ipv4/ip_forward
+iptables -P FORWARD DROP
+echo "Configuraciones completadas"
