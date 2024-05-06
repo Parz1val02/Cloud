40e93f60e93a5d60ee1da2426960cd392cfca74b
r.b4rr10s <rodrigoedu11@gmail.com>
2024-05-05 22:04:49 -0500

    Script para workers

---
+++ Lab4/scriptb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+existe_ovs() {
+	ovs-vsctl br-exists "$1"
+	return $?
+}
+if ! existe_ovs "$?"; then
+	ovs-vsctl add-br "$1"
+	echo "OVS $1 creado"
+else
+	echo "OVS $1 ya existe"
+fi
+for iface in "$@"; do
+	if [ "$iface" == "$1" ]; then
+		continue
+	fi
+	ovs-vsctl add-port "$1" "$iface"
+done
+echo "Configuraciones completadas"
